2867012cc673ca2a4377c47f39ac9f4542f42029
aeueoe <drom.jul@gmail.com>
2024-07-15 18:58:20 +0200

    authentication upd

--- index.js
+++ index.js
@@ -47,6 +47,21 @@ app.get("/", (req, res) => {
   res.send("Hello from myFlix!");
 });
 
+// Return a list of ALL films
+app.get(
+  "/movies",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    await Movie.find()
+      .then((movies) => {
+        res.status(201).json(movies);
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send("Error: " + error);
+      });
+  }
+);
 
 // Return data about a single film by title
 app.get(
