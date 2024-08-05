f998660adad6b0703fa6bd542a9a7c232ffac1b5
aeueoe <drom.jul@gmail.com>
2024-08-05 10:27:39 +0200

    modified index.js

--- index.js
+++ index.js
@@ -21,7 +21,29 @@ mongoose
   });
 
 const cors = require("cors");
-app.use(cors());
+
+let allowedOrigins = [
+  "http://localhost:8080",
+  "http://testsite.com",
+  "http://localhost:1234",
+  "https://myflix-morefilms.netlify.app",
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        // If a specific origin isn’t found on the list of allowed origins
+        let message =
+          "The CORS policy for this application doesn’t allow access from origin " +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 
 // Middleware
 app.use(morgan("common"));
@@ -264,6 +286,7 @@ app.put(
     if (req.body.Username) updateFields.Username = req.body.Username;
     if (req.body.Password) updateFields.Password = req.body.Password;
     if (req.body.Email) updateFields.Email = req.body.Email;
+    if (req.body.Birthday) updateFields.Birthday = req.body.Birthday;
 
     try {
       const updatedUser = await User.findOneAndUpdate(
